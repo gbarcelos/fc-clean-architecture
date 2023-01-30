d8dae6e5b3871ca77775ced5f44978a336937715
GUSTAVO BARCELOS <cloudexatasolar@gmail.com>
2023-01-30 09:58:21 -0300

    Adicionando notification em customer

---
+++ src/domain/@shared/notification/notification.ts
@@ -0,0 +1,27 @@
+export type NotificationError = {
+    message: string;
+    context: string;
+  };
+
+  export default class Notification {
+
+    private errors: NotificationError[] = [];
+
+    addError(error: NotificationError) {
+      this.errors.push(error);
+    }
+
+    hasErrors(): boolean {
+      return this.errors.length > 0;
+    }
+
+    messages(context?: string): string {
+        let message = "";
+        this.errors.forEach((error) => {
+          if (context === undefined || error.context === context) {
+            message += `${error.context}: ${error.message},`;
+          }
+        });
+        return message;
+      }
+  }
